5be7e08acdabe575185eedb729f5354007e4ff95
Luisa Pallares <pallares@princeton.edu>
2020-06-15 16:41:16 -0400

    mapping eQTL with MatrixEQTL

---
+++ matrixeqtl.launchR.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+infofile=$1
+
+covfile=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $1 }' $infofile`
+analysis=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $2 }' $infofile`
+vst=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $3 }' $infofile`
+nor=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $4 }' $infofile`
+PCs=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $5 }' $infofile`
+file=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $6 }' $infofile`
+covfree=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $7 }' $infofile`
+
+Rscript ~/scripts/MatrixEqtl/matrixeqtl.subsets.R $covfile $analysis $vst $nor $PCs $file $covfree
+
+
